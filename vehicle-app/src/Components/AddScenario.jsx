af88d08bf533604cfd14fb16991961adfc92aafe
swapnilchavan13 <chavanswapnil822@gmail.com>
2023-05-24 23:53:23 +0530

    sidenabar

--- vehicle-app/src/Components/AddScenario.jsx
+++ vehicle-app/src/Components/AddScenario.jsx
@@ -5,7 +5,24 @@ export const AddScenario = () => {
   return (
     <div className='addmaindiv'>
         <h4>Scenario/add</h4>
+
+        <h2>Add Scenario</h2>
+        <div className='middlediv'>
+        <div>
+            <h5>Scenario Name</h5>
+            <input type="text" placeholder='Test Scenario'/>
+        </div>
+        <div>
+            <h5>Scenario Time (seconds)</h5>
+            <input type="text" placeholder='10'/>
+        </div>
+        </div>
         
+        <div className='threebuttons'>
+            <button>Add</button>
+            <button>Reset</button>
+            <button>Go Back</button>
+        </div>
     </div>
   )
 }
