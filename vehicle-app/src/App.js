90c501edadeee153e9af3b30e875d404f29725cb
swapnilchavan13 <chavanswapnil822@gmail.com>
2023-05-24 23:04:31 +0530

    sidenabar

--- vehicle-app/src/App.js
+++ vehicle-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Sidenavbar } from './Components/Sidenavbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Sidenavbar />
+      <div>
+        <h1>main Conatain</h1>
+      </div>
     </div>
   );
 }
---
+++ vehicle-app/src/Components/Sidenavbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../Styles/Sidenavbar.css"
+
+export const Sidenavbar = () => {
+
+  return (
+
+    <div className='maindiv'>
+        <h2>Home</h2>
+        <h2>Add Scenario</h2>
+        <h2>All Scenario</h2>
+        <h2>Add Vehicle</h2>
+    </div>
+  )
+}
