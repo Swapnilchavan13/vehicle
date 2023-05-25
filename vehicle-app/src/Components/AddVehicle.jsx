3b1622eff773431545d882b1f4eed1f0c184a4f3
swapnilchavan13 <chavanswapnil822@gmail.com>
2023-05-26 00:46:25 +0530

    update

--- vehicle-app/src/Components/AddVehicle.jsx
+++ vehicle-app/src/Components/AddVehicle.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React from 'react';
+import "../Styles/Addvehicle.css"
 
 export const AddVehicle = () => {
   return (
-    <div>AddVehicle</div>
+    
+    <div className="custom-component">
+      <div className="row">
+
+      <div className="input-container">
+          <h4>Scenarios List</h4>
+          <select>
+          <option value="option">Select Scenario</option>
+            <option value="option1">Option 1</option>
+            <hr />
+            <option value="option2">Option 2</option>
+            <hr />
+            <option value="option3">Option 3</option>
+            <hr />
+          </select>
+        </div>
+        <div className="input-container">
+          <h4>Vehicle Name</h4>
+          <input type="text" />
+        </div>
+        <div className="input-container">
+          <h4>Speed</h4>
+          <input type="number" />
+        </div>
+        
+      </div>
+      <div className="row">
+        <div className="input-container">
+          <h4>Position X</h4>
+          <input type="text" />
+        </div>
+        <div className="input-container">
+          <h4>Position Y</h4>
+          <input type="text" />
+        </div>
+        <div className="input-container">
+          <h4>Direction</h4>
+          <select>
+          <option value="option">Select Direction</option>
+            <option value="option1">Toward</option>
+            <hr />
+            <option value="option2">Backward</option>
+            <hr />
+            <option value="option3">Upward</option>
+            <hr />
+            <option value="option3">Downward</option>
+            <hr />
+          </select>
+        </div>
+      </div>
+    </div>
   )
 }
