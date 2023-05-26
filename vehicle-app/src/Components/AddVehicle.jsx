e2ec6bdd1384a43de2fa7fc9fdfb1b306029beda
swapnilchavan13 <chavanswapnil822@gmail.com>
2023-05-26 18:33:30 +0530

    update

--- vehicle-app/src/Components/AddVehicle.jsx
+++ vehicle-app/src/Components/AddVehicle.jsx
@@ -1,58 +1,156 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "../Styles/Addvehicle.css"
+import { Link } from 'react-router-dom';
 
 export const AddVehicle = () => {
-  return (
-    
-    <div className="custom-component">
-      <div className="row">
+  const [vehicleName, setVehicleName] = useState('');
+  const [speed, setSpeed] = useState('');
+  const [positionX, setPositionX] = useState('');
+  const [positionY, setPositionY] = useState('');
+  const [direction, setDirection] = useState('');
+  const [scenarios, setScenarios] = useState([]);
 
-      <div className="input-container">
-          <h4>Scenarios List</h4>
-          <select>
-          <option value="option">Select Scenario</option>
-            <option value="option1">Option 1</option>
-            <hr />
-            <option value="option2">Option 2</option>
-            <hr />
-            <option value="option3">Option 3</option>
-            <hr />
-          </select>
-        </div>
-        <div className="input-container">
-          <h4>Vehicle Name</h4>
-          <input type="text" />
-        </div>
-        <div className="input-container">
-          <h4>Speed</h4>
-          <input type="number" />
-        </div>
-        
+  useEffect(() => {
+    fetch('http://localhost:8080/scenarios')
+      .then(response => response.json())
+      .then(data => {
+        setScenarios(data);
+      })
+      .catch(error => {
+        console.error('Error fetching scenarios:', error);
+      });
+  }, []);
+
+  const handleAddClick = () => {
+    if (
+      !vehicleName ||
+      !speed ||
+      !positionX ||
+      !positionY ||
+      !direction
+    ) {
+      alert('All fields are required!');
+      return;
+    }
+
+    const vehicleData = {
+      vehicleName,
+      speed,
+      positionX,
+      positionY,
+      direction
+    };
+
+    fetch('http://localhost:8080/vehicle', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(vehicleData)
+    })
+      .then(response => {
+        alert('Vehicle added successfully!');
+      })
+      .catch(error => {
+        console.error('Error adding vehicle:', error);
+      });
+  };
+  const handleResetClick = () => {
+    setVehicleName('');
+    setSpeed('');
+    setPositionX('');
+    setPositionY('');
+    setDirection('');
+  };
+
+  return (
+    <>    
+      <div className='vehiclemain'>
+        <h4>Vehicle/add</h4>
+        <h2>Add Vehicle</h2>
       </div>
-      <div className="row">
+      <div className="custom-component">
+        <div className="row">
         <div className="input-container">
-          <h4>Position X</h4>
-          <input type="text" />
+            <h4>Scenarios List</h4>
+            <select>
+              <option value="option">Select Scenario</option>
+              {scenarios.map(scenario => (
+                <option key={scenario.id} value={scenario.id}>
+                  {scenario.name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="input-container">
+            <h4>Vehicle Name</h4>
+            <input
+              type="text"
+              placeholder='Target abc'
+              value={vehicleName}
+              onChange={e => setVehicleName(e.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <h4>Speed</h4>
+            <input
+              type="number"
+              placeholder='2'
+              value={speed}
+              onChange={e => setSpeed(e.target.value)}
+            />
+          </div>
         </div>
-        <div className="input-container">
-          <h4>Position Y</h4>
-          <input type="text" />
+        <div className="row2">
+          <div className="input-container">
+            <h4>Position X</h4>
+            <input
+              className='required-field'
+              placeholder='0-800'
+              type="number"
+              value={positionX}
+              onChange={e => setPositionX(e.target.value)}
+            />
+            <p className='popup'>X Position should not be {'>'} 800 and {'<'} 0</p>
+          </div>
+          <div className="input-container">
+            <h4>Position Y</h4>
+            <input
+              type="number"
+              placeholder='20'
+              value={positionY}
+              onChange={e => setPositionY(e.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <h4>Direction</h4>
+            <select
+              value={direction}
+              onChange={e => setDirection(e.target.value)}
+            >
+              <option value="option">Select Direction</option>
+              <option value="Toward">Toward</option>
+              <hr />
+              <option value="Backward">Backward</option>
+              <hr />
+              <option value="Upward">Upward</option>
+              <hr />
+              <option value="Downward">Downward</option>
+              <hr />
+            </select>
+          </div>
         </div>
-        <div className="input-container">
-          <h4>Direction</h4>
-          <select>
-          <option value="option">Select Direction</option>
-            <option value="option1">Toward</option>
-            <hr />
-            <option value="option2">Backward</option>
-            <hr />
-            <option value="option3">Upward</option>
-            <hr />
-            <option value="option3">Downward</option>
-            <hr />
-          </select>
+
+        <div className='threebuttons'>
+          <button onClick={handleAddClick}>Add</button>
+          <button onClick={handleResetClick}>Reset</button>
+          <Link to={"/"}>
+            <button id='tbutton'>Go Back</button>
+          </Link>
         </div>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
+
--- vehicle-app/src/Components/HomePage.jsx
+++ vehicle-app/src/Components/HomePage.jsx
@@ -1,7 +1,145 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import '../Styles/home.css';
 
 export const HomePage = () => {
+  const [vehicles, setVehicles] = useState([]);
+  const [editVehicle, setEditVehicle] = useState(null);
+  const [editName, setEditName] = useState('');
+  const [editPositionX, setEditPositionX] = useState(0);
+  const [editPositionY, setEditPositionY] = useState(0);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/vehicle')
+      .then(response => response.json())
+      .then(data => {
+        setVehicles(data);
+      })
+      .catch(error => {
+        console.error('Error fetching vehicle data:', error);
+      });
+  }, []);
+
+  const handleEdit = (vehicle) => {
+    setEditVehicle(vehicle);
+    setEditName(vehicle.vehicleName);
+    setEditPositionX(vehicle.positionX);
+    setEditPositionY(vehicle.positionY);
+  };
+
+  const handleSave = () => {
+    if (editVehicle) {
+      const updatedVehicle = {
+        ...editVehicle,
+        vehicleName: editName,
+        positionX: editPositionX,
+        positionY: editPositionY,
+      };
+
+      fetch(`http://localhost:8080/vehicle/${editVehicle.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedVehicle),
+      })
+        .then(response => response.json())
+        .then(data => {
+          const updatedVehicles = vehicles.map(vehicle => {
+            if (vehicle.id === data.id) {
+              return data;
+            }
+            return vehicle;
+          });
+
+          setVehicles(updatedVehicles);
+          setEditVehicle(null);
+        })
+        .catch(error => {
+          console.error('Error updating vehicle:', error);
+        });
+    }
+  };
+
+  const handleCancel = () => {
+    setEditVehicle(null);
+  };
+
+  const handleDelete = (vehicleId) => {
+    fetch(`http://localhost:8080/vehicle/${vehicleId}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        const updatedVehicles = vehicles.filter(vehicle => vehicle.id !== vehicleId);
+        setVehicles(updatedVehicles);
+      })
+      .catch(error => {
+        console.error('Error deleting vehicle:', error);
+      });
+  };
+
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <div className="container">
+        <table className="rounded-table">
+          <thead>
+            <tr>
+              <th>Vehicle ID</th>
+              <th>Vehicle Name</th>
+              <th>Position X</th>
+              <th>Position Y</th>
+              <th>Speed</th>
+              <th>Direction</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {vehicles.map(vehicle => (
+              <tr key={vehicle.id}>
+                <td>{vehicle.id}</td>
+                <td>{vehicle.vehicleName}</td>
+                <td>{vehicle.positionX}</td>
+                <td>{vehicle.positionY}</td>
+                <td>{vehicle.speed}</td>
+                <td>{vehicle.direction}</td>
+                <td>
+                  <button onClick={() => handleEdit(vehicle)}>Edit</button>
+                </td>
+                <td>
+                  <button onClick={() => handleDelete(vehicle.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      {editVehicle && (
+        <div className="edit-popup">
+          <h3>Name</h3>
+          <input
+            type="text"
+            value={editName}
+            onChange={e => setEditName(e.target.value)}
+          />
+          <h3>Position X</h3>
+          <input
+            type="number"
+            value={editPositionX}
+            onChange={e => setEditPositionX(Number(e.target.value))}
+          />
+          <h3>Position Y</h3>
+          <input
+            type="number"
+            value={editPositionY}
+            onChange={e => setEditPositionY(Number(e.target.value))}
+          />
+          <br />
+          <br />
+          <button onClick={handleSave}>Save</button>
+          <button onClick={handleCancel}>Cancel</button>
+        </div>
+      )}
+    </div>
+  );
+};
